4943e6afcd898ee974bff2debc755dd6bd879188
maycon r r alves <mayconrralves@yahoo.com.br>
2022-02-21 19:02:40 -0300

    Removed console.log on server

--- server/server.mjs
+++ server/server.mjs
@@ -9,17 +9,14 @@ const io = new Server(httpServer);
 io.on('connection', socket=>{
     socket.on('join_room', room=>{
         const numberUsers = socket.adapter.rooms.get(room) ? socket.adapter.rooms.get(room).size : 0;
-        console.log('join', numberUsers)
         if(numberUsers < 1){
             socket.join(room);
             socket.emit('created_room',{
                 room,
             });
-            console.log('socket id', socket.id)
         }else if(numberUsers < 3){
             socket.join(room);
             socket.to(room).emit('joined_room', room);
-            console.log('joined_room')
         }else{
             socket.emit('full_room', room);
         }
